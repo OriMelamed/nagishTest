c36a07c2a52cf1b0d66df91839a09cfde3773b17
OriMelamed <ori.melamed@gmail.com>
2024-08-25 19:29:55 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // …
+  build: {
+    rollupOptions: {
+      external: ['/nagishli_beta.js'],
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Example of splitting vendor files
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // Adjust the limit as per your needs
+  },
+  plugins: [
+    react(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
+      include: "**/*.svg",
+    }),
+  ],
+});
+
+
